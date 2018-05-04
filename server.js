ffd30a8ac7abf14663bad06c8122019d8e14ed62
Ricardo Viera <ricardoclean@gmail.com>
2018-05-04 11:19:35 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('public/')
+});
+
+app.listen(3000, () => {
+  console.log('palette picker is listening on 3000');
+  
+})
