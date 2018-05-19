e29ca4b139bf77c646c000da32110c280354eabc
Ricardo Viera <ricardoclean@gmail.com>
2018-05-19 17:16:08 -0600

    Adds knex, pg and migration files. Adds gitignore

---
+++ public/js/scripts.js
@@ -0,0 +1,13 @@
+
+$( 'article' ).each(function( ) {
+  $( this ).css('background-color', getRandomColor());
+});
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
